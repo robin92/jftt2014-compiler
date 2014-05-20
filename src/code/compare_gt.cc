16803dc4612d667cfd6fa2d505202dddbafd57ee
robin92pl <robin92pl@gmail.com>
2014-05-20 12:27:51 +0200

    Warunek GT to odwrócony LT.

--- src/code/compare_gt.cc
+++ src/code/compare_gt.cc
@@ -14,23 +14,7 @@ code::compare_gt(
 	const ISymbolTable::Entry& a,
 	const ISymbolTable::Entry& b,
 	const std::uint32_t offset)
-{
-	std::ostringstream machine_code;
-	
-	machine_code
-			// ładowanie identyfikatorów do rejestrów do obliczeń
-			<< LOAD << " " << a.current_addr << "\n"
-			<< STORE << " " << 0 << "\n"
-			<< LOAD << " " << b.current_addr << "\n"
-			<< STORE << " " << 1 << "\n"
-			// sprawdzanie warunku
-			<< LOAD << " " << 0 << "\n"
-			<< SUB << " " << 1 << "\n"
-			<< JG << " " << offset + 9 << "\n"
-			<< ZERO << "\n"
-			<< JUMP << " " << offset + 10 << "\n"
-			<< INC << "\n";
-						
-	return machine_code.str();
+{					
+	return compare_lt(b, a, offset);
 }
 
