25dd1c4d49b4983feedfd191bfd09df638516bfd
robin92 <robin92pl@gmail.com>
2014-05-23 18:21:24 +0200

    Zoptymalizowano dodawanie jedynki.
    See #10

--- src/code/add.cc
+++ src/code/add.cc
@@ -3,6 +3,7 @@
 #include <sstream>
 #include <string>
 
+#include "config.hh"
 #include "code.hh"
 
 
@@ -14,10 +15,31 @@ code::add(const ISymbolTable::Entry& a, const ISymbolTable::Entry& b)
 {
 	std::ostringstream machine_code;
 
-	machine_code
-			<< LOAD << " " << a.current_addr << "\n"
-			<< ADD << " " << b.current_addr << "\n";
-
+	bool aIsOne = a.has_value and a.value == "1", bIsOne = b.has_value and b.value == "1";
+	if (F_ADD_ONE and (aIsOne or bIsOne))
+	{
+		// optymalizacja: dodwananie jedynki
+		std::cerr << ">> optymalizacja: +1\n";
+		
+		// right - jedynka
+		const ISymbolTable::Entry *left = &a, *right = &b;
+		if (aIsOne)
+		{
+			left = &b;
+			right = &a;
+		}
+		
+		machine_code
+				<< LOAD << " " << left->current_addr << "\n"
+				<< INC << "\n";
+	}
+	else
+	{
+		machine_code
+				<< LOAD << " " << a.current_addr << "\n"
+				<< ADD << " " << b.current_addr << "\n";
+	}
+	
 	return machine_code.str();
 }
 
