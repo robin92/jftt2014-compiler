6e34d77d63385f0814f98d7e597686d93b1ad453
robin92pl <robin92pl@gmail.com>
2014-05-30 08:17:03 +0200

    Zoptymalizowano warunek x > 0.
    See #3.

--- src/code/compare_gt.cc
+++ src/code/compare_gt.cc
@@ -3,6 +3,7 @@
 #include <sstream>
 #include <string>
 
+#include "config.hh"
 #include "code.hh"
 
 
@@ -14,7 +15,27 @@ code::compare_gt(
 	const ISymbolTable::Entry& a,
 	const ISymbolTable::Entry& b,
 	const std::uint32_t offset)
-{					
-	return compare_lt(b, a, offset);
+{	
+	std::ostringstream machine_code;
+	
+	bool bIsZero = b.has_value and (std::int32_t) b.value.find_first_not_of('0') == -1;				
+	if (F_COND_GT_ZERO and bIsZero)
+	{
+		// optymalizacja: x > 0
+		std::cerr << ">> optymalizacja: > 0\n";
+		
+		machine_code
+				<< LOAD << " " << a.current_addr << "\n"
+				<< JG << " " << offset + 4 << "\n"			// 	if a > 0
+				<< ZERO << "\n"								// 		false
+				<< JUMP << " " << offset + 5 << "\n"
+				<< INC << "\n";								// 		true
+	}
+	else
+	{
+		machine_code << compare_lt(b, a, offset);
+	}
+	
+	return machine_code.str();
 }
 
